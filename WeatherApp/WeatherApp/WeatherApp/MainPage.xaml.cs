dbb600b8579414625607657a5dc1ddc5230ac573
Nam-Hyun-Woo-korea <skadudgh1341@gmail.com>
2017-04-29 22:54:50 +0900

    dd
    dd

--- WeatherApp/WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -18,8 +18,15 @@ namespace WeatherApp
 		{
 
 			now_dust.Text = address.Text + "미세먼지";
-			var result = await API.API_SK_WeatherPlanet.getFineDust(new Data.Location(37.5714100000, 126.9658000000));
+			//var result = await API.API_SK_WeatherPlanet.getFineDust(new Data.Location(37.5714100000, 126.9658000000));
+			//dust.Text = result.grade;
+			var tmp = await API.API_SK_Tmap.searchRegions(address.Text);
+			var loc = await API.API_SK_Tmap.getLoction(tmp.First());
+
+			var result = await API.API_SK_WeatherPlanet.getFineDust(loc);
+
 			dust.Text = result.grade;
+
 		}
 
 		public async void test()
